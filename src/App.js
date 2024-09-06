12fa9f0d4c10d415145e8a0b8556786beb7d1f8f
dhalfks <miran5093@gmail.com>
2024-09-06 10:05:11 +0900

    day02

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Headers from './components/Headers';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Comp1 from './components/Comp1';
+import Comp2 from './components/Comp2';
+import Comp3 from './components/Comp3';
+import Param1 from './components/Param1';
+import Param2 from './components/Param2';
+import Param3 from './components/Param3';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Headers />
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/comp1' element={<Comp1 />} />
+            <Route path='/comp2' element={<Comp2 />} />
+            <Route path='/comp3' element={<Comp3 />} />
+            <Route path='/param/:id/:name' element={<Param1 />} />
+            <Route path='/param' element={<Param2 />} />
+            <Route path='/param3/:id/:pw' element={<Param3 />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
